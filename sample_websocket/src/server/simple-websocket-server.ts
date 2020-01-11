6e67f7147b762a5bb8a64bd31a67ff6a5cc3803c
Sergey Karasyov <sergeyvkarasyov@gmail.com>
2020-01-11 23:35:01 +0100

    sample websocket app

--- sample_websocket/src/server/simple-websocket-server.ts
+++ sample_websocket/src/server/simple-websocket-server.ts
@@ -6,9 +6,8 @@ const app = express();
 
 // HTTP Server
 app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '../../public/simple-websocket-client.html')));
+    res.sendFile(path.join(__dirname, '../public/simple-websocket-client.html')));
     //path.join(__dirname,'..','..','build',simple-websocket-client.html')
-
 const httpServer = app.listen(8000, 'localhost', () => {
     console.log('HTTP server is listening on localhost:8000');
 });
@@ -19,20 +18,9 @@ console.log('WebSocket server is listening on localhost:8085');
 
 wsServer.on('connection',
     wsClient => {
-        wsClient.send('This message was pushed by the WebSocket server');
+        wsClient.send('Sample message via websocket is sent');
 
         wsClient.onerror = (error) =>
             console.log(`The server received: ${error['code']}`);
     }
 );
-
-
-
-
-
-// Broadcasting to all clients
-/*
-wsServer.on('connection',
-    websocket => wsServer.clients
-        .forEach(
-            client =>client.send('This message was pushed by the WebSocket server')));*/
