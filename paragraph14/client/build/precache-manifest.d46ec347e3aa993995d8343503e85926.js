461fa664327616db08ce44f7f85206fad0f47c00
Sergey Karasyov <sergeyvkarasyov@gmail.com>
2020-03-17 23:50:47 +0100

    react app

---
+++ paragraph14/client/build/precache-manifest.d46ec347e3aa993995d8343503e85926.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e203334d40c68858e87ab83b106b3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30c62a95b7110c457df2",
+    "url": "/static/css/main.b77d175d.chunk.css"
+  },
+  {
+    "revision": "3cfb472fc4552e811de8",
+    "url": "/static/js/2.c62e2415.chunk.js"
+  },
+  {
+    "revision": "30c62a95b7110c457df2",
+    "url": "/static/js/main.5273fa36.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
